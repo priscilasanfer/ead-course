95caffc177df9ee52f1ec134f219faa536e067b0
Priscila Ferreira <priscilasanfer@gmail.com>
2022-09-16 20:47:44 -0300

    Comit inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# EAD Course
+
+
---
+++ src/main/java/com/ead/course/models/CourseModel.java
@@ -0,0 +1,52 @@
+package com.ead.course.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.ead.course.enums.CourseLevel;
+import com.ead.course.enums.CourseStatus;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "TB_COURSES")
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class CourseModel implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID courseId;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 250)
+    private String description;
+
+    @Column
+    private String imageUrl;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime creationDate;
+
+    @Column(nullable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime lasUpdateDate;
+
+    @Enumerated(EnumType.STRING)
+    private CourseStatus courseStatus;
+
+    @Enumerated(EnumType.STRING)
+    private CourseLevel courseLevel;
+
+    @Column(nullable = false)
+    private UUID userInstructor;
+
+}
