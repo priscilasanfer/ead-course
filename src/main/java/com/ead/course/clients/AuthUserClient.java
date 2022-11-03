8b15e73fecdd6089f6a32d93965c26b3982c0b06
Priscila Ferreira <priscilasanfer@gmail.com>
2022-11-03 14:00:51 -0300

    Comunicação sincrona - Inscrição de User em Couse - Parte 4

--- src/main/java/com/ead/course/clients/AuthUserClient.java
+++ src/main/java/com/ead/course/clients/AuthUserClient.java
@@ -1,5 +1,6 @@
 package com.ead.course.clients;
 
+import com.ead.course.dtos.CourseUserDto;
 import com.ead.course.dtos.ResponsePageDto;
 import com.ead.course.dtos.UserDto;
 import com.ead.course.services.UtilsService;
@@ -61,5 +62,15 @@ public class AuthUserClient {
         String url = REQUEST_URL_AUTHUSER + "/users/" + userId;
 
         return restTemplate.exchange(url, HttpMethod.GET, null, UserDto.class);
+    }
+
+    public void postSubscriptionUserInCourse(UUID courseId, UUID userId) {
+        String url = REQUEST_URL_AUTHUSER + "/users/" + userId + "/courses/subscription";
+
+        var courseUserDto = new CourseUserDto();
+        courseUserDto.setCourseId(courseId);
+        courseUserDto.setUserId(userId);
+
+        restTemplate.postForObject(url, courseUserDto, String.class);
     }
 }
