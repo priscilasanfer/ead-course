39c29994af99da80b3b3ca1c0592952dea25a274
Priscila Ferreira <priscilasanfer@gmail.com>
2022-11-02 23:34:12 -0300

    Comunicação sincrona - Inscrição de User em Couse - Parte 1

--- src/main/java/com/ead/course/controllers/CourseUserController.java
+++ src/main/java/com/ead/course/controllers/CourseUserController.java
@@ -1,7 +1,12 @@
 package com.ead.course.controllers;
 
 import com.ead.course.clients.CourseClient;
+import com.ead.course.dtos.SubscriptionDto;
 import com.ead.course.dtos.UserDto;
+import com.ead.course.models.CourseModel;
+import com.ead.course.models.CourseUserModel;
+import com.ead.course.services.CourseService;
+import com.ead.course.services.CourseUserService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -13,8 +18,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+import java.util.Optional;
 import java.util.UUID;
 
 @Log4j2
@@ -25,6 +34,12 @@ public class CourseUserController {
     @Autowired
     CourseClient courseClient;
 
+    @Autowired
+    CourseService courseService;
+
+    @Autowired
+    CourseUserService courseUserService;
+
     @GetMapping("/courses/{courseId}/users")
     public ResponseEntity<Page<UserDto>> getAllUsersByCourse(
             @PageableDefault(page = 0, size = 10, sort = "userId", direction = Sort.Direction.ASC) Pageable pageable,
@@ -34,6 +49,28 @@ public class CourseUserController {
         return ResponseEntity
                 .status(HttpStatus.OK)
                 .body(courseClient.getAllUsersByCourse(courseId, pageable));
+    }
+
+    @PostMapping("/courses/{courseId}/users/subscription")
+    public ResponseEntity<Object> saveSubscriptionUserInCoure(
+            @RequestBody @Valid SubscriptionDto subscriptionDto,
+            @PathVariable(value = "courseId") UUID courseId) {
+        Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
+
+        if (courseModelOptional.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found.");
+        }
+
+        if (courseUserService.existsByCourseAndUserId(courseModelOptional.get(), subscriptionDto.getUserId())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Error: subscription already exists");
+        }
+
+
+        // verificar user
+
+        CourseUserModel courseUserModel = courseUserService.save(courseModelOptional.get().convertToCourseUserModel(subscriptionDto.getUserId()));
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("Subscription created successfully.");
     }
 
 }
